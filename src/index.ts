913624a0e37770226ac2f3670f39dda56fc70a48
Jasmine <jasmine@engr2-4-65-dhcp.int.colorado.edu>
2019-05-21 12:59:28 -0600

    pulling info from pull request for backend

--- src/index.ts
+++ src/index.ts
@@ -7,6 +7,20 @@ export = (app: Application) => {
     });
 
 
-    app.on('push', async (context) => {
+    app.on('pull_request', async (context) => {
+
+        const pull_number: number = context.payload.pull_request.number;
+
+        const repo = context.payload.pull_request.head.repo;
+        const repo_owner: string = repo.owner.login;
+        const repo_name: string = repo.name;
+
+        const commits = await context.github.pullRequests.listCommits({
+            owner: repo_owner,
+            repo: repo_name,
+            number: pull_number
+        });
+
+        const commit_hashes = commits.data.map(commit => commit.sha);
     });
 }
